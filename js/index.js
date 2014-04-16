92b4866fcce5c99b6e3676281aba8526a56c9613
Andrew Millman's update for Stanford <andrew_millman@mcvsd.org>
2014-04-16 10:25:20 -0700

    Properly center the card icons

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
 
+  $(window).resize(function() {
+    centerPreviews();
+  });
+
   //remove delay for the cards once they finish appearing
   var delay = 0;
   $(".card").each(function(){
     $(this).css("-webkit-transition-delay", delay/1000 + "s").removeClass("hidden");
     delay += 200;
+    centerPreviews();
   }, function(){
     delay = 0;
   });
@@ -17,6 +22,12 @@ $(document).ready(function() {
   });
 
 });
+
+function centerPreviews() {
+  var height = $(".card").height() - 60;
+  var previewHeight  = $("img.preview").height();
+  $("img.preview").css("paddingTop", (height - previewHeight)/2 );
+}
 
 function openContent() {
   $(".row, .card-extend-back, .card-extend-back > .card-extend").addClass("open");
