9e5d2c7b5271cce5975b744b3076ce7bfc6dfb7f
Andrew Millman's update for Stanford <andrew_millman@mcvsd.org>
2014-04-16 10:21:48 -0700

    Add the content page after user clicks on card

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
 
-  var del = 0;
+  //remove delay for the cards once they finish appearing
+  var delay = 0;
   $(".card").each(function(){
-    $(this).css('-webkit-transition-delay', del/1000 + 's').removeClass("hidden");
-    del += 200;
+    $(this).css("-webkit-transition-delay", delay/1000 + "s").removeClass("hidden");
+    delay += 200;
   }, function(){
-    del = 0;
+    delay = 0;
   });
+  setTimeout(function() {
+    $(".card").css("-webkit-transition-delay", "");
+  }, 1000);
+
+  $(".card").click(function() {
+    openContent();
+  });
+
 });
+
+function openContent() {
+  $(".row, .card-extend-back, .card-extend-back > .card-extend").addClass("open");
+  $(".card-extend-back .card-extend .close-button").on('click', function(){ closeContent() });
+}
+
+function closeContent() {
+  $(".row, .card-extend-back, .card-extend-back > .card-extend").removeClass("open");
+}
